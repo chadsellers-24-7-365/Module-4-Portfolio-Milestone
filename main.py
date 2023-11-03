133a4fa8f60dcfb3d75a45ef4dbdef4c81fd3ac0
CHAD <chad.sellers@csuglobal.edu>
2023-11-03 06:03:31 -0400

    Module 4: Portfolio Milestone

---
+++ main.py
@@ -0,0 +1,29 @@
+class ItemToPurchase:
+    def __init__(self, item_name="none", item_price=0, item_quantity=0):
+        self.item_name = item_name
+        self.item_price = float(item_price)
+        self.item_quantity = int(item_quantity)
+
+    def print_item_cost(self):
+        item_total_cost = self.item_price * self.item_quantity
+        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price} = ${item_total_cost}")
+        return item_total_cost
+
+
+# Example usage:
+if __name__ == "__main__":
+    item1 = ItemToPurchase()
+    print("Item 1")
+    item1.item_name = input("Enter the item name: ")
+    item1.item_price = float(input("Enter the item price: "))
+    item1.item_quantity = int(input("Enter the item quantity: "))
+
+    item2 = ItemToPurchase()
+    print("\nItem 2")
+    item2.item_name = input("Enter the item name: ")
+    item2.item_price = float(input("Enter the item price: "))
+    item2.item_quantity = int(input("Enter the item quantity: "))
+
+    print("\nTOTAL COST")
+    total_cost = item1.print_item_cost() + item2.print_item_cost()
+    print(f"Total: ${total_cost}")
